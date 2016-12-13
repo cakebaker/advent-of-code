f8fa7bbdd4a8da2f098dcfb74c66f0f63dd7d673
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2016-12-13 14:59:21 +0100

    Add solution for second puzzle of day 2

--- 2016/02/Main.hs
+++ 2016/02/Main.hs
@@ -8,19 +8,30 @@ main = do
   content <- readFile filename
 
   let instructions = map (\xs -> map parse xs) $ lines content
-  let resultPuzzle1 = getBathroomCode keypad (2,2) instructions
 
+  let resultPuzzle1 = getBathroomCode keypadPuzzle1 (2,2) instructions
   putStrLn $ "Bathroom code is: " ++ resultPuzzle1
+
+  let resultPuzzle2 = getBathroomCode keypadPuzzle2 (1,3) instructions
+  putStrLn $ "Correct bathroom code is: " ++ resultPuzzle2
 
 
 type Position = (Int, Int)
 type Keypad = Array (Int, Int) Char
 
 
-keypad :: Keypad
-keypad = array ((1,1), (3,3)) [((1,1), '1'), ((2,1), '2'), ((3,1), '3'),
-                               ((1,2), '4'), ((2,2), '5'), ((3,2), '6'),
-                               ((1,3), '7'), ((2,3), '8'), ((3,3), '9')]
+keypadPuzzle1 :: Keypad
+keypadPuzzle1 = array ((1,1), (3,3)) [((1,1), '1'), ((2,1), '2'), ((3,1), '3'),
+                                      ((1,2), '4'), ((2,2), '5'), ((3,2), '6'),
+                                      ((1,3), '7'), ((2,3), '8'), ((3,3), '9')]
+
+
+keypadPuzzle2 :: Keypad
+keypadPuzzle2 = array ((1,1), (5,5)) [((1,1), '-'), ((2,1), '-'), ((3,1), '1'), ((4,1), '-'), ((5,1), '-'),
+                                      ((1,2), '-'), ((2,2), '2'), ((3,2), '3'), ((4,2), '4'), ((5,2), '-'),
+                                      ((1,3), '5'), ((2,3), '6'), ((3,3), '7'), ((4,3), '8'), ((5,3), '9'),
+                                      ((1,4), '-'), ((2,4), 'A'), ((3,4), 'B'), ((4,4), 'C'), ((5,4), '-'),
+                                      ((1,5), '-'), ((2,5), '-'), ((3,5), 'D'), ((4,5), '-'), ((5,5), '-')]
 
 
 parse :: Char -> Keypad -> Position -> Position
@@ -50,6 +61,7 @@ isInBounds :: Keypad -> Position -> Bool
 isInBounds k (x,y)
   | x < minBound || x > maxBound = False
   | y < minBound || y > maxBound = False
+  | k ! (x,y) == '-'             = False
   | otherwise                    = True
   -- assumption: keypads are squares
   where minBound = fst $ fst $ bounds k

22a2699d0e651997096e845549ab8a447b6e3c3d
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2016-12-19 15:39:02 +0100

    Rename fn

--- 2016/19/Main.hs
+++ 2016/19/Main.hs
@@ -3,15 +3,15 @@
 main :: IO ()
 main = do
   let elves = 3004953
-  let resultPuzzle1 = getElveWithMostPresents [(i, 1) | i <- [1..elves]]
-  putStrLn $ "The elve with the most presents is: " ++ show resultPuzzle1
+  let resultPuzzle1 = getElveWithAllPresents [(i, 1) | i <- [1..elves]]
+  putStrLn $ "The elve with all presents is: " ++ show resultPuzzle1
 
 
-getElveWithMostPresents :: [(Int, Int)] -> Int
-getElveWithMostPresents ((elve, _):[]) = elve
-getElveWithMostPresents xs
-  | even $ length xs = getElveWithMostPresents $ takePresents xs
-  | otherwise        = getElveWithMostPresents $ (last xs) : (takePresents $ init xs)
+getElveWithAllPresents :: [(Int, Int)] -> Int
+getElveWithAllPresents ((elve, _):[]) = elve
+getElveWithAllPresents xs
+  | even $ length xs = getElveWithAllPresents $ takePresents xs
+  | otherwise        = getElveWithAllPresents $ (last xs) : (takePresents $ init xs)
 
 
 takePresents :: [(Int, Int)] -> [(Int, Int)]

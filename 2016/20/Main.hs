c334c632fec96cbafc84a0fbe7a3e0ff2f9b2bd6
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2016-12-20 10:21:34 +0100

    Add solution for second puzzle of day 20

--- 2016/20/Main.hs
+++ 2016/20/Main.hs
@@ -3,15 +3,25 @@ import Data.Char (isDigit)
 import Data.List (sortBy)
 
 
+maxIP :: Int
+maxIP = 4294967295
+
+minIP :: Int
+minIP = 0
+
+
 main :: IO ()
 main = do
   [filename] <- getArgs
   content <- readFile filename
 
   let rules = sortBy sortByLowerBound $ map parse $ lines content
-  let resultPuzzle1 = findLowestNonBlockedIP rules 0
 
+  let resultPuzzle1 = findLowestNonBlockedIP rules minIP
   putStrLn $ "Lowest non-blocked IP is: " ++ show resultPuzzle1
+
+  let resultPuzzle2 = getAllowedIPCount rules
+  putStrLn $ "The blacklist allows " ++ show resultPuzzle2 ++ " IPs"
 
 
 findLowestNonBlockedIP :: [(Int, Int)] -> Int -> Int
@@ -19,6 +29,24 @@ findLowestNonBlockedIP ((lowerBound, upperBound):rules) ip
   | ip < lowerBound  = ip
   | ip > upperBound  = findLowestNonBlockedIP rules ip
   | ip >= lowerBound = findLowestNonBlockedIP rules (succ upperBound)
+
+
+getAllowedIPCount :: [(Int, Int)] -> Int
+getAllowedIPCount rules = countAllowedIPs 0 $ normalizeRules rules
+
+
+countAllowedIPs :: Int -> [(Int, Int)] -> Int
+countAllowedIPs counter ((_, upper):[])                   = counter + (maxIP - upper)
+countAllowedIPs counter ((_, upperA):(lowerB, upperB):xs) = countAllowedIPs (counter + (lowerB - upperA - 1)) ((lowerB, upperB):xs)
+
+
+normalizeRules :: [(Int, Int)] -> [(Int, Int)]
+normalizeRules [] = []
+normalizeRules (x:[]) = (x:[])
+normalizeRules ((lowerA, upperA):(lowerB, upperB):xs)
+  | upperA > lowerB && upperA < upperB = normalizeRules ((lowerA, upperB):xs)
+  | upperA > lowerB && upperA > upperB = normalizeRules ((lowerA, upperA):xs)
+  | upperA < lowerB                    = (lowerA, upperA) : normalizeRules ((lowerB, upperB):xs)
 
 
 parse :: String -> (Int, Int)

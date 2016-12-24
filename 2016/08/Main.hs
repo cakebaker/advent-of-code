4b42411849b1e6ef694282a79b83f70c7289dc32
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2016-12-24 17:30:50 +0100

    Add solution for second puzzle of day 8

--- 2016/08/Main.hs
+++ 2016/08/Main.hs
@@ -13,6 +13,9 @@ main = do
   let resultPuzzle1 = getLitPixelCount initialScreen instructions
   putStrLn $ "There are " ++ show resultPuzzle1 ++ " pixels lit"
 
+  putStrLn $ "Screen for puzzle 2:"
+  putStr $ toString $ getFinalScreen initialScreen instructions
+
 
 type Screen = Array (Int, Int) Bool
 
@@ -29,8 +32,11 @@ initialScreen = array ((0,0), (rows, cols)) [((i, j), False) | i <- [0..rows], j
 
 
 getLitPixelCount :: Screen -> [(Screen -> Screen)] -> Int
-getLitPixelCount initialScreen instructions = length $ filter id $ elems finalScreen
-                                              where finalScreen = foldl (\screen f -> f screen) initialScreen instructions
+getLitPixelCount initialScreen instructions = length $ filter id $ elems $ getFinalScreen initialScreen instructions
+
+
+getFinalScreen :: Screen -> [(Screen -> Screen)] -> Screen
+getFinalScreen initialScreen instructions = foldl (\screen f -> f screen) initialScreen instructions
 
 
 parse :: String -> (Screen -> Screen)
@@ -67,3 +73,13 @@ rotateRow :: Int -> Int -> Screen -> Screen
 rotateRow row offset sc = sc // [((row, i), vs !! i) | i <- [0..(screenWidth - 1)]]
                           where vs   = take screenWidth $ drop (screenWidth - offset) $ foldr1 (++) $ repeat vals
                                 vals = [sc ! (row, i) | i <- [0..(screenWidth - 1)]]
+
+
+toString :: Screen -> String
+toString scr = unlines $ chunkOf screenWidth $ map boolToChar [scr ! (i,j) | i <- [0..(screenHeight - 1)], j <- [0..(screenWidth - 1)]]
+               where boolToChar = (\x -> if x then '#' else '.')
+
+
+chunkOf :: Int -> [a] -> [[a]]
+chunkOf _ [] = []
+chunkOf n xs = take n xs : (chunkOf n $ drop n xs)

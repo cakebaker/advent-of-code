42fc6c3a506bc71faefff689cb58e2b9191ba176
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2016-12-26 16:10:59 +0100

    Rewrite solution for first puzzle of day 1

--- 2016/01/Main.hs
+++ 2016/01/Main.hs
@@ -9,47 +9,49 @@ main = do
   content <- readFile filename
 
   let instructions = map parse $ words $ head $ lines content
-  let result1 = getDistanceToHQ instructions
+  let resultPuzzle1 = getDistanceToHQ instructions
 
-  putStrLn $ "Distance to Easter Bunny HQ: " ++ show result1 ++ " blocks"
+  putStrLn $ "Distance to Easter Bunny HQ: " ++ show resultPuzzle1 ++ " blocks"
 
 
 data RelativeDirection = Left | Right deriving (Show, Eq)
 data Direction = North | East | South | West deriving (Show)
-data Instruction = Instruction { relativeDirection :: RelativeDirection, blocks :: Int } deriving (Show)
+type Instruction = (RelativeDirection, Int)
+type Position = (Int, Int, Direction)
 
 
 parse :: String -> Instruction
 parse (x:xs)
-  | x == 'L' = Instruction Left blocks
-  | otherwise = Instruction Right blocks
+  | x == 'L' = (Left, blocks)
+  | x == 'R' = (Right, blocks)
   where blocks = read $ takeWhile isDigit xs
 
 
 getDistanceToHQ :: [Instruction] -> Int
-getDistanceToHQ instructions = (abs $ fst destination) + (abs $ snd destination)
-                               where destination = move (0, 0) North instructions
+getDistanceToHQ instructions = abs x + abs y
+                               where (x, y, _) = foldr move (0, 0, North) (reverse instructions)
 
 
-move :: (Int, Int) -> Direction -> [Instruction] -> (Int, Int)
-move position _ [] = position
-move (x, y) North (instruction:instructions)
-  | rDirection == Left = move (x - b, y) West instructions
-  | otherwise          = move (x + b, y) East instructions
-  where rDirection = relativeDirection instruction
-        b          = blocks instruction
-move (x, y) East (instruction:instructions)
-  | rDirection == Left = move (x, y + b) North instructions
-  | otherwise          = move (x, y - b) South instructions
-  where rDirection = relativeDirection instruction
-        b          = blocks instruction
-move (x, y) South (instruction:instructions)
-  | rDirection == Left = move (x + b, y) East instructions
-  | otherwise          = move (x - b, y) West instructions
-  where rDirection = relativeDirection instruction
-        b          = blocks instruction
-move (x, y) West (instruction:instructions)
-  | rDirection == Left = move (x, y - b) South instructions
-  | otherwise          = move (x, y + b) North instructions
-  where rDirection = relativeDirection instruction
-        b          = blocks instruction
+move :: Instruction -> Position -> Position
+move instruction@(relativeDir, blocks) position@(x, y, dir) = case dir of
+                                                                North -> moveHorizontal instruction position
+                                                                South -> moveHorizontal (toggleRelativeDirection relativeDir, blocks) position
+                                                                West  -> moveVertical instruction position
+                                                                East  -> moveVertical (toggleRelativeDirection relativeDir, blocks) position
+
+
+moveHorizontal :: Instruction -> Position -> Position
+moveHorizontal (relativeDir, blocks) (x, y, _) = case relativeDir of
+                                                     Left  -> (x - blocks, y, West)
+                                                     Right -> (x + blocks, y, East)
+
+
+moveVertical :: Instruction -> Position -> Position
+moveVertical (relativeDir, blocks) (x, y, _) = case relativeDir of
+                                                     Left  -> (x, y - blocks, South)
+                                                     Right -> (x, y + blocks, North)
+
+
+toggleRelativeDirection :: RelativeDirection -> RelativeDirection
+toggleRelativeDirection Left  = Right
+toggleRelativeDirection Right = Left

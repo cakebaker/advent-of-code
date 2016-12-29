fa68b926f28ce148510bb23197b29d35744376fc
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2016-12-29 10:37:46 +0100

    Add solution for first puzzle of day 17

---
+++ 2016/17/Main.hs
@@ -0,0 +1,81 @@
+import Prelude hiding (Left, Right)
+import Data.Hash.MD5
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
+
+data Direction = Up | Down | Left | Right
+
+instance Show Direction where
+  show Up    = "U"
+  show Down  = "D"
+  show Left  = "L"
+  show Right = "R"
+
+
+type Position = (Int, Int)
+
+
+targetPosition :: Position
+targetPosition = (3,3)
+
+
+gridSize :: Int
+gridSize = 4
+
+
+main :: IO ()
+main = do
+  let passcode = "mmsxrhfx"
+
+  let resultPuzzle1 = findShortestPath passcode (0,0)
+  putStrLn $ "The shortest path is: " ++ resultPuzzle1
+
+
+findShortestPath :: String -> Position -> String
+findShortestPath passcode position = map head $ map show $ head $ sortBy (comparing length) $ find passcode [] position
+
+
+find :: String -> [Direction] -> Position -> [[Direction]]
+find passcode directions position
+  | position == targetPosition = [directions]
+  | otherwise = do
+                  direction <- getNextDirections passcode directions position
+                  find passcode (directions ++ [direction]) (getNewPosition position direction)
+
+
+getNextDirections :: String -> [Direction] -> Position -> [Direction]
+getNextDirections passcode directions position = possibleDirections
+                                                 where allDirections = [Up, Down, Left, Right]
+                                                       path = map head $ map show directions
+                                                       hash = generateHash (passcode ++ path)
+                                                       possibleDirections = map (\(dir, _) -> dir) $
+                                                                            filter (\(dir, _) -> isValidPosition $ getNewPosition position dir) $
+                                                                            filter (\(_, open) -> open) $
+                                                                            zipWith (\dir c -> (dir, isDoorOpen c)) allDirections hash
+
+
+getNewPosition :: Position -> Direction -> Position
+getNewPosition (x,y) Up    = (x, y - 1)
+getNewPosition (x,y) Down  = (x, y + 1)
+getNewPosition (x,y) Left  = (x - 1, y)
+getNewPosition (x,y) Right = (x + 1, y)
+
+
+isDoorOpen :: Char -> Bool
+isDoorOpen c
+  | elem c "bcdef" = True
+  | otherwise      = False
+
+
+isValidPosition :: Position -> Bool
+isValidPosition (x,y)
+  | x >= 0 && 
+    x < gridSize &&
+    y >= 0 && 
+    y < gridSize    = True
+  | otherwise       = False
+
+
+generateHash :: String -> String
+generateHash s = take 4 $ md5s $ Str s

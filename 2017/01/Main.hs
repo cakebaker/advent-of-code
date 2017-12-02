ef56c260b4bec19c0cfa3e305ca01bc4bf8fbb1f
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-02 15:18:49 +0100

    Remove unnecessary param of sumMatching fn (day 1)

--- 2017/01/Main.hs
+++ 2017/01/Main.hs
@@ -9,16 +9,16 @@ main = do
   let captcha = map digitToInt $ head $ lines content
   let infiniteCaptcha = cycle captcha
 
-  let resultPuzzle1 = sumMatching captcha (tail infiniteCaptcha) 0
+  let resultPuzzle1 = sumMatching captcha (tail infiniteCaptcha)
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
 
   let offset = length captcha `div` 2
-  let resultPuzzle2 = sumMatching captcha (drop offset infiniteCaptcha) 0
+  let resultPuzzle2 = sumMatching captcha (drop offset infiniteCaptcha)
   putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
 
 
-sumMatching :: [Int] -> [Int] -> Int -> Int
-sumMatching [] _ total = total
-sumMatching (x:xs) (y:ys) total
-  | x == y    = sumMatching xs ys (total + x)
-  | otherwise = sumMatching xs ys total
+sumMatching :: [Int] -> [Int] -> Int
+sumMatching [] _ = 0
+sumMatching (x:xs) (y:ys)
+  | x == y    = x + sumMatching xs ys
+  | otherwise = sumMatching xs ys

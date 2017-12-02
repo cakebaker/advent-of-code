67b743198a76fdb2cf44826c7f30ae3ca2c605c1
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-02 14:34:02 +0100

    Add solution for puzzle 2 of day 2

--- 2017/02/Main.hs
+++ 2017/02/Main.hs
@@ -12,9 +12,19 @@ main = do
   let resultPuzzle1 = sum $ map calcChecksum rows
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
 
+  let resultPuzzle2 = sum $ map calcEvenlyDivisible rows
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
+
+
+stringsToInts :: [String] -> [Int]
+stringsToInts = map (\s -> read s :: Int)
 
 calcChecksum :: [Int] -> Int
 calcChecksum xs = maximum xs - minimum xs
 
-stringsToInts :: [String] -> [Int]
-stringsToInts = map (\s -> read s :: Int)
+calcEvenlyDivisible :: [Int] -> Int
+calcEvenlyDivisible xs = fst evenlyDivisiblePair `div` snd evenlyDivisiblePair
+                         where evenlyDivisiblePair = head $ filter isEvenlyDivisible [(x, y) | x <- xs, y <- xs, x /= y]
+
+isEvenlyDivisible :: (Int, Int) -> Bool
+isEvenlyDivisible (x, y) = x `mod` y == 0

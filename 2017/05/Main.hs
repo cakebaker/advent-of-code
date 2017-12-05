0669eec89ba075efec25b946c2afa3b55068bf89
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-05 10:46:35 +0100

    Add solution for puzzle 2 of day 5

--- 2017/05/Main.hs
+++ 2017/05/Main.hs
@@ -9,17 +9,25 @@ main = do
   let instructions = listArray (0, (length instructions) - 1) instructions
                      where instructions = map (\s -> read s :: Int) $ lines content
 
-  let resultPuzzle1 = countSteps instructions
+  let resultPuzzle1 = countSteps instructions succ
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
 
+  let resultPuzzle2 = countSteps instructions calcOffset
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
 
-countSteps :: Array Int Int -> Int
-countSteps a = move (a, initialPos) initialCount
-               where initialPos   = 0
-                     initialCount = 0
 
-move :: (Array Int Int, Int) -> Int -> Int
-move (a, pos) acc
+countSteps :: Array Int Int -> (Int -> Int) -> Int
+countSteps a f = move (a, initialPos) f initialCount
+                 where initialPos   = 0
+                       initialCount = 0
+
+move :: (Array Int Int, Int) -> (Int -> Int) -> Int -> Int
+move (a, pos) offsetFn acc
   | pos > (snd $ bounds a) = acc
-  | otherwise = move (a // [(pos, succ v)], pos + v) (succ acc)
-                where v = a ! pos
+  | otherwise              = move (a // [(pos, offsetFn v)], pos + v) offsetFn (succ acc)
+                             where v = a ! pos
+
+calcOffset :: Int -> Int
+calcOffset o
+  | o >= 3    = pred o
+  | otherwise = succ o

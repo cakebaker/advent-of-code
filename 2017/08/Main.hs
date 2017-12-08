aa5a6c7d8e2c2b6f393987d5093a5d0a9a334814
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-08 16:58:00 +0100

    Add solution for puzzle 2 of day 8

--- 2017/08/Main.hs
+++ 2017/08/Main.hs
@@ -6,10 +6,13 @@ main = do
   [filename] <- getArgs
   content <- readFile filename
 
-  let instructions = lines content
+  let instructions = map parse $ lines content
 
-  let resultPuzzle1 = solve $ map parse instructions
+  let resultPuzzle1 = solve instructions
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
+
+  let resultPuzzle2 = solve2 instructions
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
 
 
 type Instruction = (String, Int, String, (Int -> Bool))
@@ -18,6 +21,11 @@ solve :: [Instruction] -> Int
 solve instructions = maximum $ registerValues
                      where registerValues = Map.elems $ execute instructions registers
                            registers      = Map.empty
+
+solve2 :: [Instruction] -> Int
+solve2 instructions = execute' instructions registers highestValue
+                      where registers    = Map.empty
+                            highestValue = 0
 
 parse :: String -> Instruction
 parse s = (register, offset, condRegister, condFn)
@@ -46,6 +54,14 @@ execute (x:xs) m
   | executeCondition m condRegister condFn      = execute xs (Map.insertWith (+) register value m)
   | otherwise                                   = execute xs m
   where (register, value, condRegister, condFn) = x
+
+execute' :: [Instruction] -> Map.Map String Int -> Int -> Int
+execute' [] m max = max
+execute' (x:xs) m max
+  | executeCondition m condRegister condFn      = execute' xs updated (maximum [max, updated Map.! register])
+  | otherwise                                   = execute' xs m max
+  where (register, value, condRegister, condFn) = x
+        updated                                 = Map.insertWith (+) register value m
  
 executeCondition :: Map.Map String Int -> String -> (Int -> Bool) -> Bool
 executeCondition m register f

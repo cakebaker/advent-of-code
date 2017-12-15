0a6ed8885493db615dd5548ec20289f259536a16
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-15 16:08:32 +0100

    Add solution for puzzle 2 of day 15

--- 2017/15/Main.hs
+++ 2017/15/Main.hs
@@ -2,11 +2,19 @@ import Data.Bits ((.&.))
 
 main :: IO ()
 main = do
-  let generatorA = iterate (generate factorA) 783
-  let generatorB = iterate (generate factorB) 325
+  let inputA = 783
+  let inputB = 325
+
+  let generatorA  = iterate (generate factorA) inputA
+  let generatorB  = iterate (generate factorB) inputB
+  let generatorA' = iterate (generate' divisibleBy4 factorA) inputA
+  let generatorB' = iterate (generate' divisibleBy8 factorB) inputB
 
   let resultPuzzle1 = judge $ take 40000000 $ zip generatorA generatorB
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
+
+  let resultPuzzle2 = judge $ take 5000000 $ zip generatorA' generatorB'
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
 
 
 factorA :: Int
@@ -20,6 +28,18 @@ divisor = 2147483647
 
 generate :: Int -> Int -> Int
 generate multiplier x = (x * multiplier) `rem` divisor
+
+generate' :: (Int -> Bool) -> Int -> Int -> Int
+generate' cond multiplier x
+  | cond result = result
+  | otherwise   = generate' cond multiplier result
+  where result = generate multiplier x
+
+divisibleBy4 :: Int -> Bool
+divisibleBy4 x = x .&. 3 == 0
+
+divisibleBy8 :: Int -> Bool
+divisibleBy8 x = x .&. 7 == 0
 
 judge :: [(Int, Int)] -> Int
 judge pairs = length $ filter matchLowest16Bits pairs

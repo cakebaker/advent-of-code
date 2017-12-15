35f3cd96dc7db133943bb955dbcb7333c7704250
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-15 15:18:32 +0100

    Refactor solution for puzzle 1 of day 15

--- 2017/15/Main.hs
+++ 2017/15/Main.hs
@@ -1,26 +1,28 @@
-import Data.Int (Int16, Int64)
+import Data.Bits ((.&.))
 
 main :: IO ()
 main = do
-  let generatorA = generate factorA 783
-  let generatorB = generate factorB 325
+  let generatorA = iterate (generate factorA) 783
+  let generatorB = iterate (generate factorB) 325
 
-  let resultPuzzle1 = length $ filter (\(a,b) -> toInt16 a == toInt16 b) $ take 40000000 $ zip generatorA generatorB
+  let resultPuzzle1 = judge $ take 40000000 $ zip generatorA generatorB
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
 
 
-factorA :: Int64
+factorA :: Int
 factorA = 16807
 
-factorB :: Int64
+factorB :: Int
 factorB = 48271
 
-divisor :: Int64
+divisor :: Int
 divisor = 2147483647
 
-generate :: Int64 -> Int64 -> [Int64]
-generate multiplier x = y:generate multiplier y
-                        where y = rem (x * multiplier) divisor
+generate :: Int -> Int -> Int
+generate multiplier x = (x * multiplier) `rem` divisor
 
-toInt16 :: Int64 -> Int16
-toInt16 = fromIntegral
+judge :: [(Int, Int)] -> Int
+judge pairs = length $ filter matchLowest16Bits pairs
+
+matchLowest16Bits :: (Int, Int) -> Bool
+matchLowest16Bits (a, b) = a .&. 0xFFFF == b .&. 0xFFFF

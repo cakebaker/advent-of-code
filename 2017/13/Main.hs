80901d7d4d930555ec7c5ccb6f2217260d83f856
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-12-29 08:40:05 +0100

    Add solution for puzzle 2 of day 13

--- 2017/13/Main.hs
+++ 2017/13/Main.hs
@@ -12,6 +12,9 @@ main = do
   let resultPuzzle1 = calculateSeverity $ filter isCaught layers
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
 
+  let resultPuzzle2 = findDelay 0 $ map toScanner layers
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
+
 
 isCaught :: (Int, Int) -> Bool
 isCaught (depth, range) = depth `mod` repetition == 0
@@ -19,6 +22,16 @@ isCaught (depth, range) = depth `mod` repetition == 0
 
 calculateSeverity :: [(Int, Int)] -> Int
 calculateSeverity caught = foldl (\acc (depth, range) -> acc + (depth * range)) 0 caught
+
+toScanner :: (Int, Int) -> [Bool]
+toScanner (depth, range) = drop depth $ cycle (True:(replicate notCaught False))
+                           where notCaught = (2 * (range - 1)) - 1
+
+findDelay :: Int -> [[Bool]] -> Int
+findDelay counter xs
+  | isNotCaught = counter
+  | otherwise   = findDelay (counter + 1) (map tail xs)
+  where isNotCaught = all (\x -> head x == False) xs
 
 layer :: ReadP (Int, Int)
 layer = do

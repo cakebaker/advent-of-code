029acc0d412f65971b75e11cf9b323302cd5fc41
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2018-12-01 16:40:56 +0100

    Add solution for puzzle 2 of day 1

--- 2018/01/Main.hs
+++ 2018/01/Main.hs
@@ -1,15 +1,27 @@
 import System.Environment
+import qualified Data.IntSet as IntSet
 
 main :: IO ()
 main = do
   [filename] <- getArgs
   content <- readFile filename
 
-  let resultPuzzle1 = sum $ map parse $ lines content
+  let frequencyChanges = map parse $ lines content
 
+  let resultPuzzle1 = sum $ frequencyChanges
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
+
+
+  let frequencies = scanl (+) 0 $ cycle $ frequencyChanges
+  let resultPuzzle2 = firstDuplicate IntSet.empty frequencies
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
 
 
 parse :: String -> Int
 parse ('+':s) = read s
 parse ('-':s) = 0 - read s
+
+firstDuplicate :: IntSet.IntSet -> [Int] -> Int
+firstDuplicate processed (x:xs)
+  | IntSet.member x processed = x
+  | otherwise                 = firstDuplicate (IntSet.insert x processed) xs

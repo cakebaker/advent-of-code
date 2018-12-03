a26cbff03da390bb0e0703782897ccd97bdc463d
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2018-12-03 07:41:00 +0100

    Add solution for puzzle 2 of day 2

--- 2018/02/Main.hs
+++ 2018/02/Main.hs
@@ -11,6 +11,9 @@ main = do
   let resultPuzzle1 = checksum boxIDs
   putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
 
+  let resultPuzzle2 = common boxIDs
+  putStrLn $ "Result of puzzle 2: " ++ show resultPuzzle2
+
 
 checksum :: [String] -> Int
 checksum xs = twoLetterCount * threeLetterCount
@@ -19,3 +22,15 @@ checksum xs = twoLetterCount * threeLetterCount
 
 hasXLetters :: Int -> String -> Bool
 hasXLetters l s = not . null $ filter (\xs -> length xs == l) $ group $ sort s
+
+common :: [String] -> String
+common boxIDs = head $ filter (\x -> length x == lengthOfCorrectBoxIDs) commonLetters
+                where combinations          = [(x,y) | x <- boxIDs, y <- boxIDs, x < y]
+                      commonLetters         = map (\(x,y) -> positionalIntersect x y) combinations
+                      lengthOfCorrectBoxIDs = (length $ head boxIDs) - 1
+
+positionalIntersect :: String -> String -> String
+positionalIntersect [] [] = []
+positionalIntersect (x:xs) (y:ys)
+  | x == y    = x:positionalIntersect xs ys
+  | otherwise = positionalIntersect xs ys

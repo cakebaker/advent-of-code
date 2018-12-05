ad8c5a7153e256555f8e343bdbfe1e6b4dfb4942
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2018-12-05 14:45:08 +0100

    Add solution for puzzle 1 of day 5

---
+++ 2018/05/Main.hs
@@ -0,0 +1,41 @@
+import System.Environment
+import Data.Char (isUpper, toLower, toUpper)
+
+main :: IO ()
+main = do
+  [filename] <- getArgs
+  content <- readFile filename
+
+  let units = head $ lines content
+
+  let resultPuzzle1 = length $ process units
+  putStrLn $ "Result of puzzle 1: " ++ show resultPuzzle1
+
+
+process :: [Char] -> [Char]
+process [] = []
+process xs
+  | hasOppositePolarities xs = process $ removeOppositePolarities xs
+  | otherwise                = xs
+
+hasOppositePolarities :: [Char] -> Bool
+hasOppositePolarities []     = False
+hasOppositePolarities (a:[]) = False
+hasOppositePolarities (a:b:xs)
+  | isOppositePolarity a b = True
+  | otherwise              = hasOppositePolarities (b:xs)
+
+removeOppositePolarities :: [Char] -> [Char]
+removeOppositePolarities []     = []
+removeOppositePolarities (a:[]) = [a]
+removeOppositePolarities (a:b:xs)
+  | isOppositePolarity a b = removeOppositePolarities xs
+  | otherwise              = a:removeOppositePolarities (b:xs)
+
+isOppositePolarity :: Char -> Char -> Bool
+isOppositePolarity c1 c2 = toOppositePolarity c1 == c2
+
+toOppositePolarity :: Char -> Char
+toOppositePolarity c
+  | isUpper c = toLower c
+  | otherwise = toUpper c

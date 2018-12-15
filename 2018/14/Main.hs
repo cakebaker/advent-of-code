79a0ada0d3377abd2f138541910530d0cf41abf6
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2018-12-15 15:25:47 +0100

    Add solution for puzzle 2 of day 14

--- 2018/14/Main.hs
+++ 2018/14/Main.hs
@@ -9,11 +9,20 @@ main = do
   let resultPuzzle1 = solvePuzzle1 puzzleInput initialRecipes
   putStrLn $ "The result of puzzle 1 is: " ++ show resultPuzzle1
 
+  let resultPuzzle2 = solvePuzzle2 puzzleInput initialRecipes
+  putStrLn $ "The result of puzzle 2 is: " ++ show resultPuzzle2
+
 
 solvePuzzle1 :: Int -> ((Int, Int), Seq Int) -> String
-solvePuzzle1 n recipes = toString $ lastTen $ head $ dropWhile (\(_,xs) -> Seq.length xs < maxRecipes) $ iterate nextRecipes recipes
+solvePuzzle1 n recipes = toString $ lastN 10 $ snd $ head $ dropWhile (\(_,xs) -> Seq.length xs < maxRecipes) $ iterate nextRecipes recipes
                          where maxRecipes = n + 10
                                toString   = \x -> foldl1 (++) $ map show $ toList x
+
+solvePuzzle2 :: Int -> ((Int, Int), Seq Int) -> Int
+solvePuzzle2 n recipes = Seq.length foundRecipes - Seq.length needle - offset
+                         where foundRecipes = snd $ head $ dropWhile (\(_,xs) -> not $ containsNearEnd needle xs) $ iterate nextRecipes recipes
+                               needle       = Seq.fromList $ splitIntoDigits n
+                               offset       = getOffset needle foundRecipes
 
 nextRecipes :: ((Int, Int), Seq Int) -> ((Int, Int), Seq Int)
 nextRecipes ((posA, posB), recipes) = ((newPosA, newPosB), allRecipes)
@@ -23,8 +32,24 @@ nextRecipes ((posA, posB), recipes) = ((newPosA, newPosB), allRecipes)
                                             newPosA    = mod (posA + valA + 1) (Seq.length allRecipes)
                                             newPosB    = mod (posB + valB + 1) (Seq.length allRecipes)
 
-lastTen :: ((a, a), Seq a) -> Seq a
-lastTen (_, xs) = snd $ Seq.splitAt (Seq.length xs - 10) xs
+containsNearEnd :: Seq Int -> Seq Int -> Bool
+containsNearEnd needle haystack
+  | needle == patternAtEnd          = True
+  | needle == patternAtOneBeforeEnd = True
+  | otherwise                       = False
+  where haystackLen           = Seq.length haystack
+        needleLen             = Seq.length needle
+        patternAtEnd          = snd $ Seq.splitAt (haystackLen - needleLen) haystack
+        patternAtOneBeforeEnd = Seq.take needleLen $ snd $ Seq.splitAt (haystackLen - needleLen - 1) haystack
+
+getOffset :: Seq Int -> Seq Int -> Int
+getOffset needle haystack
+  | needle == patternAtEnd = 0
+  | otherwise              = 1
+  where patternAtEnd = lastN (Seq.length needle) haystack
+
+lastN :: Int -> Seq a -> Seq a
+lastN n xs = snd $ Seq.splitAt (Seq.length xs - n) xs
 
 splitIntoDigits :: Int -> [Int]
 splitIntoDigits x = map (\x -> read [x] :: Int) (show x)

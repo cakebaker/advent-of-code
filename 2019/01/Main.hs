49d71568826c77df4ca6e42071c8210e10809c01
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-01 16:52:42 +0100

    Add solution for puzzle 2 of day 1

--- 2019/01/Main.hs
+++ 2019/01/Main.hs
@@ -7,12 +7,21 @@ main = do
 
   let masses = map toInt $ lines content
   
-  let requiredFuel = sum $ map calculateFuel masses
-  putStrLn $ "Result of puzzle 1: " ++ show requiredFuel
+  let result1 = sum $ map calculateFuel masses
+  putStrLn $ "Result of puzzle 1: " ++ show result1
+
+  let result2 = sum $ map (\x -> calculateTotalFuel x 0) masses
+  putStrLn $ "Result of puzzle 2: " ++ show result2
 
 
 calculateFuel :: Int -> Int
 calculateFuel x = (div x 3) - 2
+
+calculateTotalFuel :: Int -> Int -> Int
+calculateTotalFuel x fuelCount
+  | fuel <= 0 = fuelCount
+  | otherwise = calculateTotalFuel fuel (fuelCount + fuel)
+  where fuel  = calculateFuel x
 
 toInt :: String -> Int
 toInt s = read s

65a395279498647d503091faaab1907424fa8b83
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-03 10:45:01 +0100

    Add solution for puzzle 2 of day 2

--- 2019/02/Main.hs
+++ 2019/02/Main.hs
@@ -10,12 +10,20 @@ main = do
   content <- readFile filename
 
   let intCodes = head $ map (fst . last) $ map (readP_to_S codes) $ lines content
-  let program = V.fromList intCodes
-  let initialInstructionPointer = 0
+  let program  = V.fromList intCodes
 
   let result1 = V.head $ execute initialInstructionPointer $ program V.// [(1, 12), (2, 2)]
   putStrLn $ "Result of puzzle 1: " ++ show result1
 
+  let nounAndVerbs = [(noun, verb) | noun <- [0..99], verb <- [0..99]]
+  let wantedOutput = 19690720
+  let (noun, verb) = findInputs nounAndVerbs wantedOutput program
+  let result2      = 100 * noun + verb
+  putStrLn $ "Result of puzzle 2: " ++ show result2
+
+
+initialInstructionPointer :: Int
+initialInstructionPointer = 0
 
 execute :: Int -> Vector Int -> Vector Int
 execute instructionPointer memory
@@ -31,6 +39,14 @@ executeOpcode :: [Int] -> (Int -> Int -> Int) -> Vector Int -> Vector Int
 executeOpcode (a:b:target:[]) op memory = memory V.// [(target, op valA valB)]
                                           where valA = memory V.! a
                                                 valB = memory V.! b
+
+findInputs :: [(Int, Int)] -> Int -> Vector Int -> (Int, Int)
+findInputs ((noun, verb):xs) wantedOutput memory
+  | V.head result == wantedOutput = (result V.! nounAddress, result V.! verbAddress)
+  | otherwise                     = findInputs xs wantedOutput memory
+  where result      = execute initialInstructionPointer $ memory V.// [(nounAddress, noun), (verbAddress, verb)]
+        nounAddress = 1
+        verbAddress = 2
 
 codes :: ReadP [Int]
 codes = do

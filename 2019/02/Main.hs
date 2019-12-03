6a4098ff65bce3b36975762644a264fbcd31a954
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-03 10:15:13 +0100

    Use naming from puzzle 2

--- 2019/02/Main.hs
+++ 2019/02/Main.hs
@@ -11,25 +11,26 @@ main = do
 
   let intCodes = head $ map (fst . last) $ map (readP_to_S codes) $ lines content
   let program = V.fromList intCodes
+  let initialInstructionPointer = 0
 
-  let result1 = V.head (execute 0 $ program V.// [(1, 12), (2, 2)])
+  let result1 = V.head $ execute initialInstructionPointer $ program V.// [(1, 12), (2, 2)]
   putStrLn $ "Result of puzzle 1: " ++ show result1
 
 
 execute :: Int -> Vector Int -> Vector Int
-execute programPointer program
-  | opcode == 1 = execute nextProgramPointer (executeOpcode positions (+) program)
-  | opcode == 2 = execute nextProgramPointer (executeOpcode positions (*) program)
-  | otherwise   = program
-  where opcode             = program V.! programPointer
-        nextProgramPointer = programPointer + codeLength
-        positions          = V.toList $ V.tail $ V.slice programPointer codeLength program
-        codeLength         = 4
+execute instructionPointer memory
+  | opcode == 1 = execute nextInstructionPointer $ executeOpcode parameters (+) memory
+  | opcode == 2 = execute nextInstructionPointer $ executeOpcode parameters (*) memory
+  | otherwise   = memory
+  where opcode                 = memory V.! instructionPointer
+        nextInstructionPointer = instructionPointer + instructionLength
+        parameters             = V.toList $ V.tail $ V.slice instructionPointer instructionLength memory
+        instructionLength      = 4
 
 executeOpcode :: [Int] -> (Int -> Int -> Int) -> Vector Int -> Vector Int
-executeOpcode (a:b:target:[]) op program = program V.// [(target, op valA valB)]
-                                           where valA = program V.! a
-                                                 valB = program V.! b
+executeOpcode (a:b:target:[]) op memory = memory V.// [(target, op valA valB)]
+                                          where valA = memory V.! a
+                                                valB = memory V.! b
 
 codes :: ReadP [Int]
 codes = do

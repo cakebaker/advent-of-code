b555832fe97af719bb41a154a867e85c4df84fbe
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-13 10:43:48 +0100

    Make input fn more flexible (day 5)

--- 2019/05/Main.hs
+++ 2019/05/Main.hs
@@ -18,48 +18,48 @@ main = do
   let intCodes = head $ map (fst . last) $ map (readP_to_S codes) $ lines content
   let program  = V.fromList intCodes
 
-  let (_, _, result1) = exec inputValuePuzzle1 (initialInstructionPointer, program, [])
+  let (_, _, _, result1) = exec (initialInstructionPointer, program, [inputValuePuzzle1], [])
   mapM_ print result1
 
-  let (_, _, result2) = exec inputValuePuzzle2 (initialInstructionPointer, program, [])
+  let (_, _, _, result2) = exec (initialInstructionPointer, program, [inputValuePuzzle2], [])
   mapM_ print result2
 
 
 type InstructionPointer = Int
 type Memory             = Vector Int
+type Input              = [Int]
 type Output             = [Int]
-type ProgramState       = (InstructionPointer, Memory, Output)
+type ProgramState       = (InstructionPointer, Memory, Input, Output)
 
 pattern PositionMode  = 0
 pattern ImmediateMode = 1
 
-exec :: Int -> ProgramState -> ProgramState
-exec inputValue programState@(instructionPointer, memory, _)
-  | opcode == 1 = execFn $ execOp parameterModes (+) programState
-  | opcode == 2 = execFn $ execOp parameterModes (*) programState
-  | opcode == 3 = execFn $ input inputValue programState
-  | opcode == 4 = execFn $ output mode1stParam programState
-  | opcode == 5 = execFn $ jmpIfTrue parameterModes programState
-  | opcode == 6 = execFn $ jmpIfFalse parameterModes programState
-  | opcode == 7 = execFn $ lessThan parameterModes programState
-  | opcode == 8 = execFn $ equals parameterModes programState
+exec :: ProgramState -> ProgramState
+exec programState@(instructionPointer, memory, _, _)
+  | opcode == 1 = exec $ execOp parameterModes (+) programState
+  | opcode == 2 = exec $ execOp parameterModes (*) programState
+  | opcode == 3 = exec $ input programState
+  | opcode == 4 = exec $ output mode1stParam programState
+  | opcode == 5 = exec $ jmpIfTrue parameterModes programState
+  | opcode == 6 = exec $ jmpIfFalse parameterModes programState
+  | opcode == 7 = exec $ lessThan parameterModes programState
+  | opcode == 8 = exec $ equals parameterModes programState
   | otherwise   = programState
   where (mode3rdParam:mode2ndParam:mode1stParam:d:e:[]) = leftPad 5 $ toDigits $ memory V.! instructionPointer
         opcode                                          = (10 * d) + e
         parameterModes                                  = [mode1stParam, mode2ndParam, mode3rdParam]
-        execFn                                          = exec inputValue
 
 execOp :: [Int] -> (Int -> Int -> Int) -> ProgramState -> ProgramState
-execOp (modeA:modeB:_:[]) op (instructionPointer, memory, out) = (instructionPointer + instructionLength, memory V.// [(target, op valA valB)], out)
-                                                                 where (a:b:target:[])   = V.toList $ V.tail $ V.slice instructionPointer instructionLength memory
-                                                                       valA              = resolveMode a modeA memory
-                                                                       valB              = resolveMode b modeB memory
-                                                                       instructionLength = 4
+execOp (modeA:modeB:_:[]) op (instructionPointer, memory, ins, outs) = (instructionPointer + instructionLength, memory V.// [(target, op valA valB)], ins, outs)
+                                                                      where (a:b:target:[])   = V.toList $ V.tail $ V.slice instructionPointer instructionLength memory
+                                                                            valA              = resolveMode a modeA memory
+                                                                            valB              = resolveMode b modeB memory
+                                                                            instructionLength = 4
 
-input :: Int -> ProgramState -> ProgramState
-input inputValue (instructionPointer, memory, out) = (instructionPointer + instructionLength, memory V.// [(target, inputValue)], out)
-                                                     where target            = V.head $ V.tail $ V.slice instructionPointer instructionLength memory
-                                                           instructionLength = 2
+input :: ProgramState -> ProgramState
+input (instructionPointer, memory, (inputValue:ins), outs) = (instructionPointer + instructionLength, memory V.// [(target, inputValue)], ins, outs)
+                                                             where target            = V.head $ V.tail $ V.slice instructionPointer instructionLength memory
+                                                                   instructionLength = 2
 
 jmpIfTrue :: [Int] -> ProgramState -> ProgramState
 jmpIfTrue modes programState = jmpIf modes (/= 0) programState
@@ -68,9 +68,9 @@ jmpIfFalse :: [Int] -> ProgramState -> ProgramState
 jmpIfFalse modes programState = jmpIf modes (== 0) programState
 
 jmpIf :: [Int] -> (Int -> Bool) -> ProgramState -> ProgramState
-jmpIf (modeA:modeB:_:[]) op programState@(instructionPointer, memory, out)
-  | op valA   = (valB, memory, out)
-  | otherwise = (instructionPointer + instructionLength, memory, out)
+jmpIf (modeA:modeB:_:[]) op programState@(instructionPointer, memory, ins, outs)
+  | op valA   = (valB, memory, ins, outs)
+  | otherwise = (instructionPointer + instructionLength, memory, ins, outs)
   where (a:b:[])          = V.toList $ V.tail $ V.slice instructionPointer instructionLength memory
         valA              = resolveMode a modeA memory
         valB              = resolveMode b modeB memory
@@ -83,19 +83,19 @@ equals :: [Int] -> ProgramState -> ProgramState
 equals modes programState = compareBy modes (==) programState
 
 compareBy :: [Int] -> (Int -> Int -> Bool) -> ProgramState -> ProgramState
-compareBy (modeA:modeB:_:[]) op(instructionPointer, memory, out)
-  | op valA valB = (instructionPointer + instructionLength, memory V.// [(c, 1)], out)
-  | otherwise    = (instructionPointer + instructionLength, memory V.// [(c, 0)], out)
+compareBy (modeA:modeB:_:[]) op(instructionPointer, memory, ins, outs)
+  | op valA valB = (instructionPointer + instructionLength, memory V.// [(c, 1)], ins, outs)
+  | otherwise    = (instructionPointer + instructionLength, memory V.// [(c, 0)], ins, outs)
   where (a:b:c:[])        = V.toList $ V.tail $ V.slice instructionPointer instructionLength memory
         valA              = resolveMode a modeA memory
         valB              = resolveMode b modeB memory
         instructionLength = 4
 
 output :: Int -> ProgramState -> ProgramState
-output mode (instructionPointer, memory, out) = (instructionPointer + instructionLength, memory, out ++ [outputValue])
-                                                where param             = V.head $ V.tail $ V.slice instructionPointer instructionLength memory
-                                                      outputValue       = resolveMode param mode memory
-                                                      instructionLength = 2
+output mode (instructionPointer, memory, ins, outs) = (instructionPointer + instructionLength, memory, ins, outs ++ [outputValue])
+                                                      where param             = V.head $ V.tail $ V.slice instructionPointer instructionLength memory
+                                                            outputValue       = resolveMode param mode memory
+                                                            instructionLength = 2
 
 resolveMode :: Int -> Int -> Memory -> Int
 resolveMode x PositionMode memory = memory V.! x

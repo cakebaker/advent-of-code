d183fa07bdbf940ee284e93997c5b3865f2e577b
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-18 13:25:46 +0100

    Add solution for puzzle 2 of day 6

--- 2019/06/Main.hs
+++ 2019/06/Main.hs
@@ -5,15 +5,20 @@ main = do
   [filename] <- getArgs
   content <- readFile filename
 
-  let centerOfMass = "COM"
   let initialLevel = 0
   let orbits = map parse $ lines content
   
   let result1 = countOrbits [centerOfMass] orbits initialLevel
   putStrLn $ "Result of puzzle 1: " ++ show result1
 
+  let result2 = orbitalTransfersBetween "YOU" "SAN" orbits
+  putStrLn $ "Result of puzzle 2: " ++ show result2
+
 
 type Orbit = (String, String)
+
+centerOfMass :: String
+centerOfMass = "COM"
 
 countOrbits :: [String] -> [Orbit] -> Int -> Int
 countOrbits [] _ _ = 0
@@ -25,6 +30,25 @@ countOrbits (x:xs) orbits currentLevel
 
 inOrbitAround :: String -> [Orbit] -> [String]
 inOrbitAround target orbits = map snd $ filter (\x -> fst x == target) orbits
+
+findFirstDuplicate :: [String] -> [String] -> String
+findFirstDuplicate (x:xs) ys
+  | elem x ys = x
+  | otherwise = findFirstDuplicate xs ys
+
+orbitalTransfersBetween :: String -> String -> [Orbit] -> Int
+orbitalTransfersBetween objectA objectB orbits = length pathAToIntersection + length pathBToIntersection
+                                                 where pathA               = pathToCenterOfMass objectA orbits
+                                                       pathB               = pathToCenterOfMass objectB orbits
+                                                       intersection        = findFirstDuplicate pathA pathB
+                                                       pathAToIntersection = takeWhile (/= intersection) pathA
+                                                       pathBToIntersection = takeWhile (/= intersection) pathB
+
+pathToCenterOfMass :: String -> [Orbit] -> [String]
+pathToCenterOfMass target orbits
+  | target == centerOfMass = []
+  | otherwise              = object:(pathToCenterOfMass object orbits)
+  where object = fst $ head $ filter (\x -> snd x == target) orbits
 
 parse :: String -> Orbit
 parse s = (takeWhile (/= ')') s, tail $ dropWhile (/= ')') s)

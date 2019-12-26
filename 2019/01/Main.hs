4ff9e5ca45793810d391616e08ea3b789b1d8342
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-26 10:53:28 +0100

    Clean up solutions for day 1

--- 2019/01/Main.hs
+++ 2019/01/Main.hs
@@ -10,17 +10,17 @@ main = do
   let result1 = sum $ map calculateFuel masses
   putStrLn $ "Result of puzzle 1: " ++ show result1
 
-  let result2 = sum $ map (\x -> calculateTotalFuel x 0) masses
+  let result2 = sum $ map calculateTotalFuel masses
   putStrLn $ "Result of puzzle 2: " ++ show result2
 
 
 calculateFuel :: Int -> Int
-calculateFuel x = (div x 3) - 2
+calculateFuel x = x `div` 3 - 2
 
-calculateTotalFuel :: Int -> Int -> Int
-calculateTotalFuel x fuelCount
-  | fuel <= 0 = fuelCount
-  | otherwise = calculateTotalFuel fuel (fuelCount + fuel)
+calculateTotalFuel :: Int -> Int
+calculateTotalFuel x
+  | fuel <= 0 = 0
+  | otherwise = fuel + calculateTotalFuel fuel
   where fuel  = calculateFuel x
 
 toInt :: String -> Int

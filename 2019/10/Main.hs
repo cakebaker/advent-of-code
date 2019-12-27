ec004e9594dab965e4386fdf9dc3f9b29484c153
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2019-12-27 10:46:00 +0100

    Add solution for puzzle 2 of day 10

--- 2019/10/Main.hs
+++ 2019/10/Main.hs
@@ -1,5 +1,5 @@
 import System.Environment
-import Data.List (nub, partition, sortOn)
+import Data.List (nub, nubBy, partition, sortOn)
 
 main :: IO ()
 main = do
@@ -11,11 +11,26 @@ main = do
   let (locationMonitoringStation, result1) = last $ sortOn snd $ countVisibleAsteroids positions positions
   putStrLn $ "Result of puzzle 1: " ++ show result1
 
+  let (x,y)   = head $ drop 199 $ visibleAsteroidsFrom locationMonitoringStation positions
+  let result2 = x * 100 + y
+  putStrLn $ "Result of puzzle 2: " ++ show result2
+
 
 type Position = (Int, Int)
 
 asteroid :: Char
 asteroid = '#'
+
+visibleAsteroidsFrom :: Position -> [Position] -> [Position]
+visibleAsteroidsFrom position@(x,y) allPositions = getVisibleFrom position q1 ++ getVisibleFrom position q2 ++ getVisibleFrom position q3 ++ getVisibleFrom position q4
+                                                   where positionsWithoutCurrent = filter (/= position) allPositions
+                                                         q1 = filter (\(x1,y1) -> x1 >= x && y1 < y) positionsWithoutCurrent
+                                                         q2 = filter (\(x1,y1) -> x1 > x && y1 >= y) positionsWithoutCurrent
+                                                         q3 = filter (\(x1,y1) -> x1 <= x && y1 > y) positionsWithoutCurrent
+                                                         q4 = filter (\(x1,y1) -> x1 < x && y1 <= y) positionsWithoutCurrent
+
+getVisibleFrom :: Position -> [Position] -> [Position]
+getVisibleFrom position positions = map fst $ sortOn snd $ nubBy (\(_,x) (_, y) -> x == y) $ map (\a -> (a, slope position a)) positions
 
 countVisibleAsteroids :: [Position] -> [Position] -> [(Position, Int)]
 countVisibleAsteroids [] _ = []
